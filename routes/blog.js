af8f03695fb84c7e37464ec1f01e0c401eca4bdc
jripke74@gmail.com <jripke74@gmail.com>
2021-12-15 08:26:08 -0800

    add logic to display single post

--- routes/blog.js
+++ routes/blog.js
@@ -11,8 +11,13 @@ router.get("/", function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts", function (req, res) {
-  res.render("posts-list");
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find({}, { title: 1, summary: 1, "author.name": 1 })
+    .toArray();
+  res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async function (req, res) {
@@ -42,6 +47,20 @@ router.post("/posts", async function (req, res) {
   const result = await db.getDb().collection("posts").insertOne(newPost);
   console.log(result);
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("post-detail", { post: post });
 });
 
 module.exports = router;
